defd143632131bfc07c0e3489790405b258a50ba
Michael Clifford <cliffom@gmail.com>
2023-07-31 17:06:29 -0700

    Current changes

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -8,8 +8,8 @@ public partial class Player : Area2D
     public Vector2 ScreenSize; // Size of the game window.
 
     /**************************************************************************
-	*** Functions
-	**************************************************************************/
+    *** Functions
+    **************************************************************************/
 
     // _Ready is called when a node enters the scene tree
     public override void _Ready()
@@ -22,37 +22,21 @@ public partial class Player : Area2D
     {
         var velocity = Vector2.Zero;
         var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        animatedSprite2D.Play();
 
-        if (Input.IsActionPressed("move_right"))
-        {
-            velocity.X += 1;
-        }
-
-        if (Input.IsActionPressed("move_left"))
-        {
-            velocity.X -= 1;
-        }
-
-        if (Input.IsActionPressed("move_down"))
-        {
-            velocity.Y += 1;
-        }
-
-        if (Input.IsActionPressed("move_up"))
-        {
-            velocity.Y -= 1;
-        }
-
-
+        if (Input.IsActionPressed("move_right")) velocity.X++;
+        if (Input.IsActionPressed("move_left")) velocity.X--;
+        if (Input.IsActionPressed("move_down")) velocity.Y++;
+        if (Input.IsActionPressed("move_up")) velocity.Y--;
 
         if (velocity.Length() > 0)
         {
             velocity = velocity.Normalized() * Speed;
-            animatedSprite2D.Play();
+            //animatedSprite2D.Play();
         }
         else
         {
-            animatedSprite2D.Stop();
+            //animatedSprite2D.Stop();
         }
 
         // Clamp the position to the screen boundaries
@@ -62,26 +46,22 @@ public partial class Player : Area2D
             y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
         );
 
-        if (velocity.X > 0)
-        {
-            animatedSprite2D.Animation = "right";
-        }
-        else if (velocity.X < 0)
-        {
-            animatedSprite2D.Animation = "left";
-        }
-        else if (velocity.Y > 0)
-        {
-            animatedSprite2D.Animation = "down";
-        }
-        else if (velocity.Y < 0)
-        {
-            animatedSprite2D.Animation = "up";
-        }
-        else
-        {
-            animatedSprite2D.Animation = "idle";
-        }
+        animatedSprite2D.Animation = updateAnimation(velocity);
     }
 
+    private StringName updateAnimation(Vector2 velocity)
+    {
+        return velocity.X switch
+        {
+            > 0 => "right",
+            < 0 => "left",
+            _ => velocity.Y switch
+            {
+                > 0 => "down",
+                < 0 => "up",
+                _ => "idle"
+            }
+
+        };
+    }
 }
